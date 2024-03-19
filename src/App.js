a60093003b28285f30630166d20925e02a4dcb2f
Odhi-ambo <michaelderrick605@gmail.com>
2024-03-19 11:53:17 +0300

    introducing Button Component, styling,tasks components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from "react"
 import Header from "./components/header";
+import Tasks from "./components/Tasks";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+        
+    {
+        id: 1,
+        text:'Weekend Kenya Rugby Champonship semis-finals ',
+        day:'Saturday 23th at 3pm',
+        reminder:true,
+    },
+    {
+        id: 2,
+        text:'Travelling back from South Coast,Diani',
+        day:'Saturday 24th at 9am',
+        reminder:true,
+    },
+    {
+        id: 3,
+        text:'Back to classes on Monday',
+        day:'Saturday 25th at 8am',
+        reminder:true,
+    },
+])
   return(
     <div className="container">
-<Header title = {1}/>
+<Header/>
+<Tasks tasks = {tasks}/>
     </div>
   )
 }
