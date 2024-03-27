2a2c9bb8ca5556652120c8dc95739dcd0d0afaf8
Odhi-ambo <michaelderrick605@gmail.com>
2024-03-27 17:13:31 +0300

    modified the package-lock.json

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] =useState(false)
   const  [tasks,setTasks] = useState([
     {
       id: 1,
@@ -24,6 +25,12 @@ const App = () => {
       reminder:true,
   },  
   ])
+    const addTask = (task) => {
+      const id = Math.floor(Math.random() * 10000) + 1
+      const newTask = {id, ...task}
+      setTasks([...tasks, newTask])
+      console.log(task);
+    }
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !==id))
   }
@@ -39,8 +46,12 @@ const App = () => {
   return(
 
     <div className="container">
-<Header/>
-<AddTask/>
+<Header 
+onAdd = {() => setShowAddTask
+  (!showAddTask)} 
+  showAdd = {showAddTask}
+  />
+{showAddTask && <AddTask onAdd = {addTask}/>}
 {tasks.length > 0? ( 
 <Tasks tasks = {tasks} 
 onDelete = {deleteTask} 
